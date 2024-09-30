89f3c3c8fa81a13093ca537d0908357b504924c4
mattaniah <23040322+mattaniahA@users.noreply.github.com>
2024-09-29 21:16:27 -0400

    fix some formatss

--- eop/interpret_eop.py
+++ eop/interpret_eop.py
@@ -4,12 +4,12 @@ from math import fabs
 
 file_path = "eop-iau2000-1846-now.json"
 with open(file_path, "r") as file:
-    eop_data = json.load(file)
+    full_eop_data = json.load(file)
 
 
 def find_closest_date(date_str, data):
     # Convert input date string to datetime object
-    input_date = datetime.strptime(date_str, "%Y-%m-%d")
+    input_date = datetime.strptime(date_str, "%Y_%m_%d")
     
     closest_date = None
     min_difference = float('inf')
@@ -17,8 +17,8 @@ def find_closest_date(date_str, data):
 
     # Iterate over the records in the JSON file to find the closest date
     for entry in data['EOP']['data']['timeSeries']:
-        record_date_str = f"{entry['time']['dateYear']}-{entry['time']['dateMonth']}-{entry['time']['dateDay']}"
-        record_date = datetime.strptime(record_date_str, "%Y-%m-%d")
+        record_date_str = f"{entry['time']['dateYear']}_{entry['time']['dateMonth']}_{entry['time']['dateDay']}"
+        record_date = datetime.strptime(record_date_str, "%Y_%m_%d")
         
         # Calculate the absolute difference between the input date and the record date
         difference = fabs((input_date - record_date).days)
@@ -31,16 +31,16 @@ def find_closest_date(date_str, data):
     return closest_date, closest_eop
 
 
+# input_date = "1998-01-19"
+input_date = "1901_05_17"
 
-input_date = "1998-01-19"
-
-closest_date, closest_eop = find_closest_date(input_date, eop_data)
+closest_date, dated_eop = find_closest_date(input_date, full_eop_data)
 
 # Print the results
-if closest_eop:
+if dated_eop:
     print(f"Input Date: {input_date}")
     print(f"Closest Date: {closest_date}")
     print("Relevant EOP Data:")
-    print(json.dumps(closest_eop, indent=4))
+    print(json.dumps(dated_eop, indent=4))
 else:
     print("No data found.")
